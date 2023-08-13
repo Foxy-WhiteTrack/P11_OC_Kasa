ccbb22dd7a3bbc1ee117900b8bfafe76474a21c3
Foxy WhiteTrack | One Trick <foxywhitetrack@protonmail.com>
2023-08-13 19:26:45 +0200

    Création de la base des routes et des pages part2

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Error404 from './components/Error404';
+import Home from './pages/Home';
+import Error404 from './pages/Error404';
 
 
 function Routes() {

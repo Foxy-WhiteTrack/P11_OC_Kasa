c996ec10e2b61b0a1b29e57f9db405b942d38b2c
Foxy WhiteTrack | One Trick <foxywhitetrack@protonmail.com>
2023-08-31 00:17:04 +0200

    Correction Layout + Link de la Nav et la classe active mis en place
    Layout sur une page indépendante
    + NavLink au lieu de Link utilisé dans la nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Error404 from './pages/Error404';
 import About from './pages/About/About';
@@ -9,26 +8,14 @@ import HousingSheet from './pages/HousingSheet/HousingSheet';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="housing/:id" element={<HousingSheet />} />
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<Error404 />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-function Layout() {
-  return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="housing/:id" element={<HousingSheet />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Error404 />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,20 +1,18 @@
 import "./Nav.css";
 import React from "react";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 
 export default function Header() {
   return (
-    <>
-      <div className="header-body">
-        <Link to="/">
-          <img src={logo} alt="Logo" />
-        </Link>
-        <nav className="nav">
-          <Link to="/" className="Link">Accueil</Link>
-          <Link to="/about" className="Link">A Propos</Link>
-        </nav>
-      </div>
-    </>
-  )
+    <div className="header-body">
+      <NavLink to="/">
+        <img src={logo} alt="Logo" />
+      </NavLink>
+      <nav className="nav">
+        <NavLink exact to="/" className="link" activeClassName="active">Accueil</NavLink>
+        <NavLink to="/about" className="link" activeClassName="active">A Propos</NavLink>
+      </nav>
+    </div>
+  );
 }
